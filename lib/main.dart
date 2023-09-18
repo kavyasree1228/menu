b7ee9edb34371f00e453adc0072e815e5e9df634
kavyasree1228 <53187741+kavyasree1228@users.noreply.github.com>
2023-09-18 09:08:55 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MenuApp());
+}
+
+class MenuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Restaurant Menu',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MenuPage(),
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Restaurant Menu'),
+      ),
+      body: const Center(
+        child: Text('Menu items to be diplayed'),
+      ),
+    );
+  }
+}
