8d57f7524f8707ed8c090ed9217852ee11f29660
kavyasree1228 <53187741+kavyasree1228@users.noreply.github.com>
2023-09-18 09:28:18 -0400

    1. added menu.json file 2. Modified main page to read from json file 3. included json in assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MenuApp());
 }
 
 class MenuApp extends StatelessWidget {
+  const MenuApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,8 +27,64 @@ class MenuPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Restaurant Menu'),
       ),
-      body: const Center(
-        child: Text('Menu items to be diplayed'),
+      body: FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('assets/menu.json'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final menuData = json.decode(snapshot.data.toString());
+            final categories = menuData['categories'];
+            return ListView.builder(
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                final category = categories[index];
+                return ListTile(
+                  title: Text(category['name']),
+                  subtitle: Text('${category['items'].length} items'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CategoryPage(category: category),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          } else {
+            return CircularProgressIndicator(); // Loading indicator
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CategoryPage extends StatelessWidget {
+  final Map<String, dynamic> category;
+
+  CategoryPage({required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    final items = category['items'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category['name']),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return ListTile(
+            title: Text(item['name']),
+            subtitle: Text('\$${item['price']}'),
+            onTap: () {
+              // Handle item press here
+              print('Pressed: ${item['name']}');
+            },
+          );
+        },
       ),
     );
   }
