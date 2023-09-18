508428d912f83f80b03585f408cd0b11ff3b02c2
kavyasree1228 <53187741+kavyasree1228@users.noreply.github.com>
2023-09-18 10:46:28 -0400

    1.added read me and made few changes to cart view to show ack message

--- README.md
+++ README.md
@@ -14,3 +14,44 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+Restaurant Menu App
+A Flutter mobile application for viewing and ordering items from a restaurant menu.
+
+Table of Contents
+Description
+
+
+Description
+The Restaurant Menu App is a mobile application built using Flutter that allows users to view a restaurant's menu, browse categories, and add items to their cart for ordering. It provides a user-friendly interface to explore the restaurant's offerings and make selections.
+
+Getting Started
+Prerequisites
+Before you begin, ensure you have met the following requirements:
+
+Flutter installed on your local machine.
+The project codebase, which can be cloned from GitHub repository link.
+Installation
+Clone the repository:
+
+bash
+Copy code
+git clone https://github.com/your-username/restaurant-menu-app.git
+Navigate to the project directory:
+
+To run the Restaurant Menu App, follow these steps:
+
+
+1. Main.dart -> displays the catagories from the menu.json file , the list of catagories along with number of items available are displayed. 
+2. Clicking on the catagory will navigate to catagory list page where the list of available options are displayed in list . 
+3. Selecting the items will add items to cart . Clicking the add to cart will show selected items. 
+4. Clicking place order will place order. 
+
+
+
+
+
+
+
+
+
--- lib/cart_view.dart
+++ lib/cart_view.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CartView extends StatelessWidget {
+class CartView extends StatefulWidget {
   final List<Map<String, dynamic>> selectedItems;
 
   CartView({required this.selectedItems});
+
+  @override
+  _CartViewState createState() => _CartViewState();
+}
+
+class _CartViewState extends State<CartView> {
+  bool orderPlaced = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.selectedItems.isNotEmpty) {
+      // If there are selected items, automatically place the order after 15 seconds.
+      Future.delayed(Duration(seconds: 15), () {
+        if (!orderPlaced) {
+          // Simulate an order placement process here.
+          setState(() {
+            orderPlaced = true;
+          });
+
+          // Simulate a delay for the order placement.
+          Future.delayed(Duration(seconds: 2), () {
+            // Reset the order placement status after a while.
+            setState(() {
+              orderPlaced = false;
+            });
+
+            // Navigate back to the main menu.
+            Navigator.of(context).pop();
+          });
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +46,82 @@ class CartView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Selected items'),
       ),
-      body: ListView.builder(
-        itemCount: selectedItems.length,
-        itemBuilder: (context, index) {
-          final item = selectedItems[index];
-          final itemName = item['name'] as String;
-          final itemPrice = item['price'] as double;
+      body: orderPlaced
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.easeInOut,
+                    height: 50,
+                    alignment: Alignment.center,
+                    color: Colors.green,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.check,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          'Your order has been placed. Please be seated\nand wait while we prepare the food.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigate back to the main menu.
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Go Back to Main Menu'),
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: widget.selectedItems.length,
+              itemBuilder: (context, index) {
+                final item = widget.selectedItems[index];
+                final itemName = item['name'] as String;
+                final itemPrice = item['price'] as double;
 
-          return ListTile(
-            title: Text(itemName),
-            subtitle: Text('\$${itemPrice.toStringAsFixed(2)}'),
-          );
-        },
-      ),
+                return ListTile(
+                  title: Text(itemName),
+                  subtitle: Text('\$${itemPrice.toStringAsFixed(2)}'),
+                );
+              },
+            ),
+      floatingActionButton: !orderPlaced
+          ? FloatingActionButton.extended(
+              onPressed: () {
+                // Simulate an order placement process.
+                setState(() {
+                  orderPlaced = true;
+                });
+
+                // Simulate a delay for the order placement.
+                Future.delayed(Duration(seconds: 15), () {
+                  // Reset the order placement status after a while.
+                  setState(() {
+                    orderPlaced = false;
+                  });
+
+                  // Navigate back to the main menu.
+                  Navigator.of(context).pop();
+                });
+              },
+              label: Text('Place Order'),
+              icon: Icon(Icons.shopping_cart),
+            )
+          : null,
     );
   }
 }
